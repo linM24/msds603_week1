8c6741f3b8082f88106106142283fc04641bb815
Lin Meng <lmeng24@hotmail.com>
2020-03-20 09:07:01 -0700

    first commit

---
+++ user_definition.py
@@ -0,0 +1,6 @@
+# https://developers.google.com/maps/documentation/javascript/get-api-key
+apikey = "AIzaSyCSX1AjP3IMYq9CsrjiAh5RqlFyBd5uJW8"
+orig_coord = '37.7909,-122.3925'
+dest_coord = '37.7765,-122.4506'
+output_file = 'time.tsv'
+bucket_name="msds694"
